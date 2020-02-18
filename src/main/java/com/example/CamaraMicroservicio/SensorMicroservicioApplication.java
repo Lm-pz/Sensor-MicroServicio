e8bd7ac4971fc50beb2e6bcf4051480818806dc3
Laura Romero <lauram.romeropz@gmail.com>
2020-02-18 13:29:21 +0100

    Primera subida

---
+++ src/main/java/com/example/CamaraMicroservicio/SensorMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.example.CamaraMicroservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorMicroservicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CamaraMicroservicio/SensorMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CamaraMicroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
